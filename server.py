9d0e192bf8f8c9e65cb5dc4637d67699335372e8
Tudor3510 <tudorcioc@gmail.com>
2022-12-07 20:15:13 +0200

    Writting the code for the socket that sends the packets which should help others identify the Among Us server

--- server.py
+++ server.py
@@ -1,10 +1,21 @@
 import socket
+import select
 
 HOST = "127.0.0.1"
 DAEMON_PORT = 45254
+
 MESSAGES_PORT = 45259
 MESSAGES_DST_PORT = 47777
+MESSAGES_MAXIMUM_BUFFER = 100
+
 MAX_DAEMON_CONNECTIONS = 1
+
+messages_clients = []
+def messages_running(messages_socket: socket):
+    if select.select([messages_socket], [], [], 0):
+        raw_message, addr = messages_socket.recvfrom(MESSAGES_MAXIMUM_BUFFER)
+        
+
 
 def start():
     daemon_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
